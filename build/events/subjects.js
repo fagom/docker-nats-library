9b92f01a8e03169fc21b947050cfcbcce319fd75
fagom174 <pradhan.subhasis174@gmail.com>
2020-11-29 19:35:10 +0530

    Updates

---
+++ build/events/subjects.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Subjects = void 0;
+var Subjects;
+(function (Subjects) {
+    Subjects["PostCreated"] = "post:created";
+    Subjects["PostDeleted"] = "post:deleted";
+    Subjects["FeedCreated"] = "feed:created";
+    Subjects["FeedUpdated"] = "feed:updated";
+    Subjects["FeedDeleted"] = "feed:deleted";
+    Subjects["VoteCreated"] = "vote:created";
+    Subjects["VoteUpdated"] = "vote:updated";
+})(Subjects = exports.Subjects || (exports.Subjects = {}));
