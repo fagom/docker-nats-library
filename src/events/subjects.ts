deadbd62a0345060de9210aa000b8015309bc664
fagom174 <pradhan.subhasis174@gmail.com>
2020-11-29 19:33:45 +0530

    Updates

---
+++ src/events/subjects.ts
@@ -0,0 +1,9 @@
+export enum Subjects {
+  PostCreated = "post:created",
+  PostDeleted = "post:deleted",
+  FeedCreated = "feed:created",
+  FeedUpdated = "feed:updated",
+  FeedDeleted = "feed:deleted",
+  VoteCreated = "vote:created",
+  VoteUpdated = "vote:updated",
+}
